fa711658d767b2fdebb37bfa874f1b9613f668f5
FloraBlack <Black1250889060@gmail.com>
2020-08-11 14:52:24 +0800

    Site updated: 2020-08-11 14:52:22

--- js/white.js
+++ js/white.js
@@ -53,3 +53,61 @@ mclose.onclick=function(){
   mmain.style.display="none";
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//设置sort的子目录隐藏显示效果
+if(document.getElementById("sort")){
+var sort=document.getElementById("sort");
+var sortdiv=document.getElementById("sort-div");
+
+sort.onmouseover=function(){
+  sortdiv.style.display="block";
+}
+sort.onmouseout=function(){
+sortdiv.onmouseover=function(){
+  console.log("wuhu");
+}
+sortdiv.onmouseout=function(){
+    sortdiv.style.display="none";
+}
+}
+}
+
+//图片懒加载
+var imgs = document.querySelectorAll('img');
+var imgdivs = document.querySelectorAll('.lazyload-img-span');
+
+
+//用来判断bound.top<=clientHeight的函数，返回一个bool值
+function isIn(el) {
+    var bound = el.getBoundingClientRect();
+    var clientHeight = window.innerHeight;
+    return bound.top <= clientHeight;
+} 
+//检查图片是否在可视区内，如果在，则加载
+function check() {
+  for(let p=0 ; p<imgs.length;p++){
+    if(isIn(imgs[p])){
+        loadImg(imgs[p]);
+        imgs[p].onload = function(){
+            let y=Number(p);
+            changeClass(y);
+        }
+    }
+}
+}
+function changeClass(num){
+  let tempse=Number(num);
+  imgdivs[tempse].className = "lazyload-img-span img-masks";
+}
+function loadImg(el) {
+    if(!el.src){
+        var source = el.dataset.src;
+        el.src = source;
+    }
+}
+
+window.onload = window.onscroll = function () { //onscroll()在滚动条滚动的时候触发
+    check();
+}

532934f339e51ede7370e512e4d03eab9e20fe35
Noooooof <Black1250889060@gmail.com>
2020-09-16 21:05:17 +0800

    Site updated: 2020-09-16 21:05:16

---
+++ font/Fontspider/spider.cmd
@@ -0,0 +1,4 @@
+@echo off
+python word.py
+Xcopy E:\Recent\MyBlog\themes\hexo-theme-white-master\source\font\.font-spider\H-TTF-BuMing-R-2.ttf E:\Recent\MyBlog\themes\hexo-theme-white-master\source\font /Y
+font-spider word.html
